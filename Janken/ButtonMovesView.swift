f9d3750af685a9677d20108c2e454ca1565d7005
Faustharus <chailloleau-damien@outlook.fr>
2023-11-19 15:49:57 +0100

    first commit

---
+++ Janken/ButtonMovesView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonMovesView.swift
+//  Janken
+//
+//  Created by Damien Chailloleau on 16/11/2023.
+//
+
+import SwiftUI
+
+struct ButtonMovesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonMovesView()
+}
