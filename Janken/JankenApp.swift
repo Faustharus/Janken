e14b75364e317a20d2296e4dcbdafadf7cf477f3
Faustharus <chailloleau-damien@outlook.fr>
2024-07-04 14:36:56 +0200

    Initial Commit

---
+++ Janken/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Janken
+//
+//  Created by Damien Chailloleau on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
